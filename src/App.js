1bdec903bec9610d7dfb7641249d08834e76cab6
hafizmp <hafiznjh007@gmail.com>
2023-01-13 22:56:45 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -35,72 +35,76 @@ function App() {
                 delay: 5500,
                 disableOnInteraction: false,
               }}
-            className="mySwiper"
+              className="mySwiper"
             >
-            <SwiperSlide>
-              <div className="curousel__one c-size">
-                <div className="prof-img">
-                  <img src={images.profileImg} alt="profile" />
+              <SwiperSlide>
+                <div className="curousel__one c-size">
+                  <div className="prof-img">
+                    <img src={images.profileImg} alt="profile" />
+                  </div>
+                  <div className="prof-bio">
+                    <h1>Hey,<br /> I'm Hafiz</h1>
+                    <span>Front-end web developer</span>
+                  </div>
                 </div>
-                <div className="prof-bio">
-                  <h1>Hey,<br /> I'm Hafiz</h1>
-                  <span>Front-end web developer</span>
-                </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="curousel__two c-size">
-                <div className="work-bio">
-                  <h1>Currently I'm in</h1>
-                  <span>Dubai, United Arab Emirates</span>
+              <SwiperSlide>
+                <div className="curousel__two c-size">
+                  <div className="work-bio">
+                    <h1>Currently I'm in</h1>
+                    <span>Dubai, United Arab Emirates</span>
+                  </div>
+                  <div className="work-img">
+                    <img src={images.workImg} alt="work" />
+                  </div>
                 </div>
-                <div className="work-img">
-                  <img src={images.workImg} alt="work" />
-                </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="curousel__three c-size">
-                <div className="bg-img">
-                  <img src={images.quoteImg} alt="quote" />
-                  <span>"I bring websites to life with clean, modern designs and intuitive functionality."</span>
+              <SwiperSlide>
+                <div className="curousel__three c-size">
+                  <div className="bg-img">
+                    <img src={images.quoteImg} alt="quote" />
+                    <span>"I bring websites to life with clean, modern designs and intuitive functionality."</span>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-
-        <div className="profile-links">
-          <a href='https://www.hafizmp.com/' target="_blank" rel="noreferrer" className="link-tab">
-            <span><BsGlobe2 size={38} /></span>
-            <h5>Visit Portfolio</h5>
-          </a>
-          <a href='https://github.com/hafizmp' target="_blank" rel="noreferrer" className="link-tab">
-            <span><BsGithub size={38} /></span>
-            <h5>View Github</h5>
-          </a>
-          <a href='https://twitter.com/Hafizmp_07' target="_blank" rel="noreferrer" className="link-tab">
-            <span><BsTwitter size={38} /></span>
-            <h5>View Twitter</h5>
-          </a>
-          <a href='https://www.linkedin.com/in/hafizmp/' target="_blank" rel="noreferrer" className="link-tab">
-            <span><BsLinkedin size={38} /></span>
-            <h5>View Linkedin</h5>
-          </a>
-          <div className="link-tab">
-            <span><RiAmazonLine size={38} /></span>
-            <h5>Visit Store</h5>
+              </SwiperSlide>
+            </Swiper>
           </div>
-          <a href='https://www.youtube.com/channel/UCF7OQFvNF-f6pYUna9nOsfw' target="_blank" rel="noreferrer" className="link-tab">
-            <span><BsYoutube size={38} /></span>
-            <h5>Youtube Channel</h5>
-          </a>
-        </div>
 
+          <div className="profile-links">
+            <a href='https://www.hafizmp.com/' target="_blank" rel="noreferrer" className="link-tab">
+              <span><BsGlobe2 size={38} /></span>
+              <h5>Visit Portfolio</h5>
+            </a>
+            <a href='https://github.com/hafizmp' target="_blank" rel="noreferrer" className="link-tab">
+              <span><BsGithub size={38} /></span>
+              <h5>View Github</h5>
+            </a>
+            <a href='https://twitter.com/Hafizmp_07' target="_blank" rel="noreferrer" className="link-tab">
+              <span><BsTwitter size={38} /></span>
+              <h5>View Twitter</h5>
+            </a>
+            <a href='https://www.linkedin.com/in/hafizmp/' target="_blank" rel="noreferrer" className="link-tab">
+              <span><BsLinkedin size={38} /></span>
+              <h5>View Linkedin</h5>
+            </a>
+            <div className="link-tab">
+              <span><RiAmazonLine size={38} /></span>
+              <h5>Visit Store</h5>
+            </div>
+            <a href='https://www.youtube.com/channel/UCF7OQFvNF-f6pYUna9nOsfw' target="_blank" rel="noreferrer" className="link-tab">
+              <span><BsYoutube size={38} /></span>
+              <h5>Youtube Channel</h5>
+            </a>
+          </div>
+
+          <div className="footer">
+            <h4>&#169; haizmp.com | 2023</h4>
+          </div>
+
+        </div>
       </div>
-    </div>
     </>
   );
 }
